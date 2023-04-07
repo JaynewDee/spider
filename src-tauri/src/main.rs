4c4ea12cf08204bffe08cfcf7a2200c9191f5ae6
NuminousBlue <jdiehl2236@gmail.com>
2023-04-07 05:22:16 -0500

    title spiders

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -14,16 +14,7 @@ use ffi::invoke_api::{
 };
 
 fn main() {
-    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-    let close = CustomMenuItem::new("close".to_string(), "Close");
-    let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
-    let menu = Menu::new()
-        .add_native_item(MenuItem::Copy)
-        .add_item(CustomMenuItem::new("hide", "Hide"))
-        .add_submenu(submenu);
-
     tauri::Builder::default()
-        .menu(menu)
         .invoke_handler(tauri::generate_handler![
             get_domains,
             get_schedule,
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,30 @@
 import React from "react";
 
+// const body = () => <div className="spider-body">{"()"}</div>;
+const left = () => <span className="">{"}"}</span>;
+const middle = () => (
+  <span className="middle-spider">
+    {"}"}
+    {/* {body()} */}
+    {"{"}
+  </span>
+);
+const right = () => <span className="">{"{"}</span>;
+
+const Spider = () => {
+  return (
+    <div className="spider">
+      {left()} {middle()} {right()}
+    </div>
+  );
+};
+
 const Header: React.FC<any> = () => {
   return (
     <header className="header-container">
-      <h2>SPIDER</h2>
+      <Spider />
+      <h2 id="app-title">SPIDER</h2>
+      <Spider />
     </header>
   );
 };
