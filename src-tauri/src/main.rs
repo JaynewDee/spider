38d963030289079446c06b32cfdcc70a1c160e89
NuminousBlue <jdiehl2236@gmail.com>
2023-04-07 04:27:52 -0500

    experiment with menus

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,12 +7,23 @@ extern crate reqwest;
 
 mod ffi;
 
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+
 use ffi::invoke_api::{
     get_dev_srcs, get_domains, get_hacker_srcs, get_reddit_srcs, get_schedule, scrape_all,
 };
 
 fn main() {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let close = CustomMenuItem::new("close".to_string(), "Close");
+    let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
+    let menu = Menu::new()
+        .add_native_item(MenuItem::Copy)
+        .add_item(CustomMenuItem::new("hide", "Hide"))
+        .add_submenu(submenu);
+
     tauri::Builder::default()
+        .menu(menu)
         .invoke_handler(tauri::generate_handler![
             get_domains,
             get_schedule,
